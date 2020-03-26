339c0e08b9768f0af0ace97751af60bd14059b89
AlexSikand <alex@sikand.org>
2020-03-26 08:39:47 -0700

    Initial Commit

---
+++ NetflixBinge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NetflixBinge
+//
+//  Created by Alex Sikand on 3/26/20.
+//  Copyright © 2020 CS411. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
